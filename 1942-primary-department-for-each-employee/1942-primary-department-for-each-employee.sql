9d91abb005b2fa6e0e52a8fee5c5d7500eeb49f8
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2024-11-19 14:32:38 +0800

    Time: 668 ms (33.47%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,3 @@
+
+select e1.employee_id, e1.department_id from employee e1 where e1.primary_flag='Y' or e1.employee_id in (select e2.employee_id from employee e2 group by e2.employee_id having count(e2.department_id)=1);
+
