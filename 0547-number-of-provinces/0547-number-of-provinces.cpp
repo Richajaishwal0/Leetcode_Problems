7127184cb95df739256d94d10f5c3b534c4754eb
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-02-11 15:02:31 +0530

    Time: 4 ms (32.37%), Space: 19.6 MB (60.59%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // perform dfs traversal
+    int findCircleNum(vector<vector<int>>& v) {
+        int n=v.size();
+        vector<bool>visited(n,false);
+       int ans=0;
+        for (int i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {   
+                ans++;
+                 queue<int>q;
+                 q.push(i);
+                 while (!q.empty())
+                 {
+                    int curr=q.front();
+                    q.pop();
+                    for (int j=0;j<n;j++)
+                    {
+                        if (v[curr][j]==1 && !visited[j])
+                        {
+                            visited[j]=true;
+                            q.push(j);
+                        }
+                    }
+                 }
+            }
+        }
+        return ans;
+    }
+};
