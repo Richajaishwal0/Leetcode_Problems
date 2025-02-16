d1ca043a6b470bbcf655bd651b7a9e578c7f874d
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-02-16 12:57:59 +0530

    Time: 5 ms (23.36%), Space: 20.8 MB (28.65%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    // perform dfs traversal
-    int findCircleNum(vector<vector<int>>& v) {
-        int n=v.size();
-        vector<bool>visited(n,false);
-       int ans=0;
-        for (int i=0;i<n;i++)
+    void Connection(int i, vector<bool> &visited, vector<vector<int>> &adjlst)
+    {
+        visited[i]=true;
+        for (int x: adjlst[i])
         {
-            if(!visited[i])
-            {   
-                ans++;
-                 queue<int>q;
-                 q.push(i);
-                 visited[i]=1;
-                 while (!q.empty())
-                 {
-                    int curr=q.front();
-                    q.pop();
-                    for (int j=0;j<n;j++)
-                    {
-                        if (v[curr][j]==1 && !visited[j])
-                        {
-                            visited[j]=true;
-                            q.push(j);
-                        }
-                    }
-                 }
+            if (!visited[x])
+            {
+                Connection(x,visited,adjlst);
             }
         }
-        return ans;
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector <vector<int>> adjlst(isConnected.size());
+        for (int i=0;i<isConnected.size();i++)
+        {
+            for (int j=0;j<isConnected[i].size();j++)
+            {
+                if(isConnected[i][j]==1 && i!=j)
+                {
+                    adjlst[i].push_back(j);
+                    adjlst[j].push_back(i);
+                }
+                
+            }
+        }
+        int count=0;
+        vector <bool> visited(adjlst.size(),false);
+        for (int i=0;i<adjlst.size();i++)
+        {
+            if (!visited[i])
+            {
+                count++;
+                Connection(i,visited,adjlst);
+            }
+        }
+        return count;
     }
 };
