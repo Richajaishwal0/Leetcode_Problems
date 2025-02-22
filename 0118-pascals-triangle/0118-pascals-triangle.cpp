385c095c12e154dc149ca849a86c1f6df27964a9
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-02-22 19:41:25 +0530

    Time: 0 ms (100%), Space: 9.8 MB (12.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-   void nCr(int n, int r, vector<int> &vec) {
-        long long res = 1;
-        vec.push_back(1); // First element is always 1
-
-        for (int i = 1; i < r; i++) {  // Fixed loop range (1 to r-1)
-            res = res * (n - i + 1) / i; // Fixed formula
-            vec.push_back(res);
-        }
-
-        if (r > 0) vec.push_back(1); // Last element is always 1 if row size > 1
-    }
-
+   vector<int> nCr(int row ){
+    long long res = 1; 
+    vector<int> ansr;
+      ansr.push_back(1);
+    // calculating nCr:
+    for (int col = 1; col< row; col++) {
+        res = res * (row - col);
+        res = res /col;
+        ansr.push_back(res);
+    }   
+    return ansr;
+   }
     vector<vector<int>> generate(int numRows) {
+        int n=numRows;
         vector<vector<int>> ans;
-
-        for (int c = 1; c <= numRows; c++) {  
-            vector<int> vec;
-            nCr(c - 1, c - 1, vec);  // Fixed function call
+        for (int c=1;c<=n;c++)
+        {   vector<int> vec=nCr(c);
             ans.push_back(vec);
         }
-
         return ans;
     }
 };
