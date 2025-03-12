ee89043615d0ac4592ef4941b8b77755a6431f7e
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-03-12 22:00:11 +0530

    Time: 25 ms (67.89%), Space: 27.4 MB (50.59%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -5,9 +5,7 @@ public:
         int m=text2.size();
         if (n==0) return 0;
         if (n==1) return text1[0]==text2[0]?1:0;
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for (int i=0;i<n;i++) dp[i][0]=0;
-        for (int j=0;j<m;j++) dp[0][j]=0;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         for (int i=1;i<=n;i++)
         {
             for (int j=1;j<=m;j++)
