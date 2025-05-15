2aca025aef8b30482e63d9e636924a66ed0c2850
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-05-15 21:42:59 +0530

    Time: 2 ms (17.24%), Space: 22.6 MB (50.15%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        vector <int> expectednums;
-        map <int, int> hashmap;
-        for (int i=0;i<nums.size();i++)
-        {
-            hashmap[nums[i]]++;
-        }
-        for (auto & it: hashmap)
+    int removeDuplicates(vector<int>& arr) {
+        // vector <int> expectednums;
+        // map <int, int> hashmap;
+        // for (int i=0;i<nums.size();i++)
+        // {
+        //     hashmap[nums[i]]++;
+        // }
+        // for (auto & it: hashmap)
 
+        // {
+        //  expectednums.push_back(it.first);
+        // }
+        // nums=expectednums;
+        // return nums.size(); NOt the good approach either can use set or two pointer approach too
+
+        // Using two pointer Approach
+        int i=0;
+        for (int j=1;j<arr.size();j++)
         {
-         expectednums.push_back(it.first);
+            if (arr[i]!=arr[j])
+            {
+                i++;
+                arr[i]=arr[j];
+            }
         }
-        nums=expectednums;
-        return nums.size();
+        return i+1;
     }
 };
