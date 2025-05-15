988adfc36e4a1a4188f88a243a739d589d16faa9
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-05-15 21:59:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
-     void reverse(vector<int>&nums,int st, int end)
-        {   
-        while (st<=end)
-        {
-            swap(nums[st],nums[end]);
-            st++;
-            end--;
-        }
-        }
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size();
-        if (n==0) return ;
-         k = k % n;
-         if (k>n) return;
-         
-        // vector <int> temp(n);
-        // if (n==0) return ;
-        // k=k%n;
-        // if (k>n){
-        //     return;
-        // }
-        // for (int i=n-k; i<n;i++)
-        // {
-        //     temp[i-(n-k)]=nums[i];
-        // }
-        // for (int i=n-1-k;i>=0;i--)
-        // {
-        //     nums[i+k]=nums[i];
-        // }
-        // for (int i=0;i<k;i++)
-        // {
-        //     nums[i]=temp[i];
-        // }
-        reverse(nums,0,n-k-1);
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
-
+        if (nums.size()==1) return;
+        vector <int> ans(nums.size(),0);
+         for (int i=0;i<k;i++)
+        {
+            ans[i]=nums[nums.size()-k+i];
+        }
+        for (int i=0;i<nums.size()-k;i++)
+        {
+            ans[i+k]=nums[i];
+        }
+       
+        nums=ans;
     }
-   
 };
