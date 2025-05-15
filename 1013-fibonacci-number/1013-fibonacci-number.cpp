c10920f496bf524ef2439e618e2b52e2b2229db4
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-05-15 12:01:27 +0530

    Time: 3 ms (64.6%), Space: 8.4 MB (11.75%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n<=1) return n;
-        vector <int> arr(n+1,0);
-        arr[0]=0;
-        arr[1]=1;
-        for (int i=2;i<=n;i++)
-        {
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-        return arr[n];
+    if (n<=1) return n;
+    vector <int> fibo(n+1,0);
+    fibo[0]=0;
+    fibo[1]=1;
+    for (int i=2;i<=n;i++)
+    {
+        fibo[i]=fibo[i-1]+ fibo[i-2];
+    }    
+    return fibo[n];
     }
 };
