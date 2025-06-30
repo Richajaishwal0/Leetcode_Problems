4254b19d25a7a03e52264186f113aa98839f0602
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-06-30 10:35:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (72.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     
     int climbStairs(int n) {
-        vector <int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+        // vector <int> dp(n+1,-1);
+        if (n==0 || n==1) return 1;
+        int prev=1;
+        int prev2=1;
+        int sum=0;
         for (int i=2;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            sum=prev + prev2;
+            prev=prev2;
+            prev2=sum;
+       
         }
-        return dp[n];
+         return sum;
     }
 };
