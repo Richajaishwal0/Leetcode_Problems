cb55a1c7df4ab4a8a1784849fcd9252df1ef1757
Richa jaishwal <156594333+Richajaishwal0@users.noreply.github.com>
2025-07-09 09:23:51 +0530

    Time: 0 ms (100%), Space: 8.9 MB (53.68%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    long long  pascal(int n, int c )
-    {
-        long long  res=1;
-        // vector<int> rowvec;
-        // rowvec.push_back(1);
-        for (int i=0;i<c;i++)
-        {
-            res*=(n-i);
-            res/=(i+1);
+    // long long  pascal(int n, int c )
+    // {
+    //     long long  res=1;
+    //     {
+    //         res*=(n-i);
+    //         res/=(i+1);
            
-        }
-        return res;
+    //     }
+    //     return res;
 
-    }
+    // }
+    // vector<int> getRow(int rowIndex) {
+    //     int n=rowIndex;
+    //     vector <int> ans;
+    //     for (int c=0;c<=n;c++)
+    //     {
+    //         ans.push_back(pascal(n,c));
+    //     }
+    //     return ans;
+
+    //  we can obtimised this code by reducing time complexity to linear
+
+
     vector<int> getRow(int rowIndex) {
-        int n=rowIndex;
+        long long  res=1;
         vector <int> ans;
-        // ans.push_back(1);
-        for (int c=0;c<=n;c++)
+        ans.push_back(res);
+        for (int i=0;i<rowIndex;i++)
         {
-            ans.push_back(pascal(n,c));
+            res= res*(rowIndex-i)/(i+1);
+            ans.push_back(res);
         }
         return ans;
     }
